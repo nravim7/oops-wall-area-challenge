cab6ecd01cb5fe81f974f4425ffca2e385a563d8
Ravi N <your.email@example.com>
2024-06-02 19:34:56 +0530

    Wall Area Challenge

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Wall wall = new Wall(5, 4);
+
+        System.out.println("Area = " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("Width = " + wall.getWidth());
+        System.out.println("Height = " + wall.getHeight());
+        System.out.println("Area = " + wall.getArea());
+    }
+}
